cb6400e365a4d685439c9e08d81b6a63c85b9c47
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2021-12-01 14:28:17 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let janaria2: game.LedSprite = null
+let pacman = game.createSprite(randint(0, 4), randint(0, 4))
+let janaria = game.createSprite(4, 2)
+let mamua = game.createSprite(2, 2)
+mamua.set(LedSpriteProperty.Blink, 100)
+mamua.set(LedSpriteProperty.Brightness, 50)
+janaria.set(LedSpriteProperty.Brightness, 10)
+basic.forever(function () {
+    if (pacman.isTouching(mamua)) {
+        game.gameOver()
+        game.setScore(1)
+    }
+    if (pacman.isTouching(janaria)) {
+        game.addScore(1)
+        janaria.delete()
+        janaria2 = game.createSprite(randint(0, 4), randint(0, 4))
+    }
+})
+basic.forever(function () {
+    basic.pause(500)
+    mamua.ifOnEdgeBounce()
+    if (input.acceleration(Dimension.X) < 200) {
+        basic.pause(200)
+        mamua.move(1)
+    }
+    if (input.acceleration(Dimension.X) < -200) {
+        mamua.move(1)
+    }
+})
